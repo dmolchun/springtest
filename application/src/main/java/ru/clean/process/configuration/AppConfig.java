9e5eab1b4e353eab13a873f944b2c201176ef012
dmolchun <d.molchun@gmail.com>
2017-10-15 20:09:10 +0500

    #8 Added authorization using spring security
    Added authorization + refactoring

--- application/src/main/java/ru/clean/process/configuration/AppConfig.java
+++ application/src/main/java/ru/clean/process/configuration/AppConfig.java
@@ -1,20 +1,31 @@
-package ru.clean.process.application;
+package ru.clean.process.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"ru.clean.process.controllers"})
-public class Application {
+@Import({AppSecurityConfig.class})
+public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public UrlBasedViewResolver setupViewResolver() {
         UrlBasedViewResolver resolver = new UrlBasedViewResolver();
         resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("");
+        resolver.setSuffix(".html");
         return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/views/**").addResourceLocations("/WEB-INF/views/");
     }
 }
---
+++ application/src/main/java/ru/clean/process/controllers/MainController.java
@@ -0,0 +1,19 @@
+package ru.clean.process.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "views/html/index";
+    }
+
+    @RequestMapping(value = "/admin")
+    public String admin() {
+        return "views/html/admin";
+    }
+
+}
